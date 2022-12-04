7d9ff573fa5a981a74b096fd5c9606c99aecbeb7
Hemyani <hdg2512@gmail.com>
2022-12-04 13:27:34 +0530

    Ability to manage Employee Wage of multiple companies using Interface approach

--- All Emp Wage Computation Problem/UC11.cs
+++ All Emp Wage Computation Problem/UC11.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 namespace All_Emp_Wage_Computation_Problem
 {
 
-    //created a class for Company emp wage
-    public class CompanyEmployeeWage
+    public class EmployeeWage
     {
         public string company;
         public int empRatePerHour;
@@ -16,7 +15,7 @@ namespace All_Emp_Wage_Computation_Problem
         public int MAX_HR_PER_MONTH;
         public int totalEmpWage;
 
-        public CompanyEmployeeWage(string company, int empRatePerHour, int NUM_OF_WORKING_DAYS, int MAX_HR_PER_MONTH)
+        public EmployeeWage(string company, int empRatePerHour, int NUM_OF_WORKING_DAYS, int MAX_HR_PER_MONTH)
         {
             this.company = company;
             this.empRatePerHour = empRatePerHour;
@@ -32,21 +31,21 @@ namespace All_Emp_Wage_Computation_Problem
             return "Total Emp Wage for company : " + this.company + " is: " + this.totalEmpWage;
         }
     }
-    public class EmpWageBuilderArray
+    public class EmpWageArray
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
 
         private int num_of_Company = 0;
-        private CompanyEmployeeWage[] companyEmpWageArray;
-        public EmpWageBuilderArray()
+        private EmployeeWage[] companyEmpWageArray;
+        public EmpWageArray()
         {
-            this.companyEmpWageArray = new CompanyEmployeeWage[7];
+            this.companyEmpWageArray = new EmployeeWage[7];
         }
         public void addCompanyEmpWage(string company, int empRatePerHour, int NUM_OF_WORKING_DAYS, int MAX_HR_PER_MONTH)
         {
-            companyEmpWageArray[this.num_of_Company] = new CompanyEmployeeWage(company, empRatePerHour, NUM_OF_WORKING_DAYS, MAX_HR_PER_MONTH);
+            companyEmpWageArray[this.num_of_Company] = new EmployeeWage(company, empRatePerHour, NUM_OF_WORKING_DAYS, MAX_HR_PER_MONTH);
             num_of_Company++;
         }
         public void computeEmpWage()
@@ -57,7 +56,7 @@ namespace All_Emp_Wage_Computation_Problem
                 Console.WriteLine(this.companyEmpWageArray[i].toString());
             }
         }
-        private int computeEmpWage(CompanyEmployeeWage companyEmpWage)
+        private int computeEmpWage(EmployeeWage companyEmpWage)
         {
             //variables
             int empHrs = 0;
