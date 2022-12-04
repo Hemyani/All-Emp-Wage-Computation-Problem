c1b0ca16e32975462df7338857655b0e36932c48
Hemyani <hdg2512@gmail.com>
2022-12-04 12:26:03 +0530

    Add project files.

---
+++ All Emp Wage Computation Problem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace All_Emp_Wage_Computation_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1.EmpAttendance();
+            //Console.ReadLine();
+
+            //UC2.EmpWage();
+            //Console.ReadLine();
+
+            //UC3.AddPartTime();
+            //Console.ReadLine();
+
+            //UC4.EmpWages();
+            //Console.ReadLine();
+
+            //UC5.EmpWages();
+            //Console.ReadLine();
+
+            //UC6.DaysOrHours();
+            //Console.ReadLine();
+
+            //UC7.Refactor();
+            //Console.ReadLine();
+
+            //UC8.computeEmpWage("DMart", 20, 30, 20);
+            //UC8.computeEmpWage("TCS", 15, 25, 25);
+            //Console.ReadLine();
+
+            //UC9 Amazon = new UC9("Amazon", 30, 25, 20);
+            //UC9 Infosys = new UC9("Infosys", 25, 22, 29);
+            //Amazon.CompanyEmpWage();
+            //Console.WriteLine(Amazon.toString());
+            //Infosys.CompanyEmpWage();
+            //Console.Write(Infosys.toString());
+
+            //UC10
+            //EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            //empWageBuilder.addCompanyEmpWage("TCS", 20, 4, 150);
+            //empWageBuilder.addCompanyEmpWage("Amazon", 25, 7, 200);
+            //empWageBuilder.addCompanyEmpWage("Infosys", 23, 3, 100);
+            //empWageBuilder.addCompanyEmpWage("Wipro", 28, 4, 160);
+            //empWageBuilder.addCompanyEmpWage("Hansen", 15, 2, 180);
+            //empWageBuilder.addCompanyEmpWage("Reliance", 25, 7, 240);
+            //empWageBuilder.addCompanyEmpWage("Apple", 40, 5, 260);
+            //empWageBuilder.computeEmpWage();
+
+            ////UC11
+            //EmpWageArray empWageBuilder = new EmpWageArray();
+            //empWageBuilder.addCompanyEmpWage("TechMahindra", 25, 7, 160);
+            //empWageBuilder.addCompanyEmpWage("TripStack", 22, 10, 230);
+            //empWageBuilder.addCompanyEmpWage("LG", 23, 3, 100);
+            //empWageBuilder.addCompanyEmpWage("Wipro", 28, 4, 160);
+            //empWageBuilder.addCompanyEmpWage("Enzigma", 15, 2, 180);
+            //empWageBuilder.addCompanyEmpWage("HCL", 25, 7, 240);
+            //empWageBuilder.addCompanyEmpWage("Samsung", 41, 10, 210);
+            //empWageBuilder.computeEmpWage();
+
+
+            ////UC14
+            //EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            //empWageBuilder.addCompanyEmpWage("TCS", 22, 3, 15);
+            //empWageBuilder.addCompanyEmpWage("Jio", 15, 3, 20);
+            //empWageBuilder.computeEmpWage();
+            //Console.WriteLine("Total wage for TCS company : " + empWageBuilder.getTotalWage("TCS"));
+            //Console.WriteLine("Total wage for Jio company : " + empWageBuilder.getTotalWage("Jio"));
+        }
+    }
+}
